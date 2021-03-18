b8adbc90cec3cde7a978d9d2b3c5fb92794fe458
Mirko_Serino <mirkos.serino@gmail.com>
2021-03-18 16:39:27 +0100

    Leaner classifier during training, all modules on top.

--- image_classifier/train.py
+++ image_classifier/train.py
@@ -1,50 +1,53 @@
-from torch import optim
 import torch
-from torchvision import datasets, transforms, models
-import torch.nn.functional as F
 from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
 import argparse
 import os
 
-parser = argparse.ArgumentParser(
-    description='Train a Neural Network on a custom image datasets')
+parser=argparse.ArgumentParser(description='Train a Neural Network on a custom image datasets')
 
 # This is the only positional argument necessarily required: the directory
 # containing training, validation and test data
 
-parser.add_argument(action='store', dest='datapath', type=str,
-                    help='Indicate the directory where the "train", "valid" and "test" folders are located')
+parser.add_argument(action='store',dest='datapath',type=str,
+help='Indicate the directory where the "train", "valid" and "test" folders are located')
 
 # Here the user can specify a directory to save the model checkpoint
-parser.add_argument('--save_dir', action='store', dest='checkpoint_save', default='bash_checkpoint',
-                    help='Directory to save your trained model checkpoint')
+parser.add_argument('--save_dir',action='store',dest='checkpoint_save',default='bash_checkpoint',
+help='Directory to save your trained model checkpoint')
 
 # Here are a few options to choose an architecture of the VGG type for the convolutional NN
 # The default value is the network used in the first part of the assignment,
 # although the classifier the user can build now is "simpler".
 
-parser.add_argument('--arch', action='store', type=str, dest='arch', default='vgg16',
-                    choices=['vgg11_bn', 'vgg13', 'vgg13_bn',
-                             'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn'],
-                    help='Choose from a given list the CNN architecture to use')
+parser.add_argument('--arch',action='store',type=str, dest='arch',default='vgg16',
+choices=['vgg11_bn','vgg13','vgg13_bn','vgg16','vgg16_bn','vgg19','vgg19_bn'],
+help='Choose from a given list the CNN architecture to use')
 
-# Here are the command line options allowing the user to specify training parameters
-parser.add_argument('--learning_rate', action='store', type=float, dest='lr', default=0.05,
-                    help='Specify the learning rate of your model to be used during training')
+# Here are the command line options allowing the user to specify training parameters 
+parser.add_argument('--learning_rate',action='store',type=float,dest='lr',default=0.05,
+help='Specify the learning rate of your model to be used during training')
 
-parser.add_argument('--epochs', action='store', type=int, dest='epochs', default=10,
-                    help='Specify the number of training epochs')
+parser.add_argument('--epochs',action='store',type=int, dest='epochs',default=10,
+help='Specify the number of training epochs')
 
-parser.add_argument('--hidden_units', action='store', type=int, dest='hidden_units', default=512,
-                    help='Specify the number of hidden units in the classifier')
+parser.add_argument('--hidden_units',action='store',type=int,dest='hidden_units',default=512,
+help='Specify the number of hidden units in the classifier')
 
 # Here the user can choose whether to use a gpu or not
 
-parser.add_argument('--gpu', action='store_true', default=False,
-                    help='Specify if you want to train your model using an available gpu')
+parser.add_argument('--gpu',action='store_true',default=False,
+help='Specify if you want to train your model using an available gpu')
 
 # Read the command line instructions and parse them
-args = parser.parse_args()
+args=parser.parse_args()
+
+#################################################
+#################################################
+#################################################
 
 # Here starts the data loading + NN training part
 # First step is to import the necessary libraries
@@ -60,23 +63,20 @@ train_transforms = transforms.Compose([transforms.RandomRotation(40),
                                        transforms.RandomResizedCrop(224),
                                        transforms.RandomHorizontalFlip(),
                                        transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406], [
-                                                            0.229, 0.224, 0.225])
-                                       ])
+                                       transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])
+                                    ])
 
 valid_transforms = transforms.Compose([transforms.Resize(224),
                                        transforms.CenterCrop(224),
                                        transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406], [
-                                                            0.229, 0.224, 0.225])
-                                       ])
+                                       transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])
+                                    ])
 
 test_transforms = transforms.Compose([transforms.Resize(224),
                                       transforms.CenterCrop(224),
                                       transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406], [
-                                                           0.229, 0.224, 0.225])
-                                      ])
+                                      transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])
+                                    ])
 
 # Load the datasets with ImageFolder
 train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
@@ -84,8 +84,7 @@ valid_data = datasets.ImageFolder(valid_dir, transform=valid_transforms)
 test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
 
 # Using the image datasets and the trainforms, define the dataloaders
-trainloader = torch.utils.data.DataLoader(
-    train_data, batch_size=64, shuffle=True)
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle = True)
 validloader = torch.utils.data.DataLoader(valid_data, batch_size=64)
 testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
 
@@ -101,32 +100,31 @@ print('Model chosen for the convolutional neural network: ' + args.arch)
 for param in model.parameters():
     param.requires_grad = False
 
-# We set up a new classifier for the network,
+# We set up a new classifier for the network, 
 # It returns the correct number of outputs and has dropout implemented
 
 # Hyperparameters for our flower_classifier: the number of hidden units (one layer) is chosen by the user
 input_size = model.classifier[0].in_features
 hidden_size = args.hidden_units
-output_size = 102
+output_size = len(train_data.class_to_idx)
 
 # Build a feed-forward network
 
 flower_classifier = nn.Sequential(nn.Linear(input_size, hidden_size),
-                                  nn.ReLU(),
-                                  nn.Dropout(0.3),
-                                  nn.Linear(hidden_size, hidden_size),
-                                  nn.ReLU(),
-                                  nn.Dropout(0.3),
-                                  nn.Linear(hidden_size, output_size),
-                                  nn.LogSoftmax(dim=1)
-                                  )
+                    nn.ReLU(),
+                    nn.Dropout(0.3),
+                    nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(),
+                    nn.Dropout(0.3),
+                    nn.Linear(hidden_size, output_size),
+                    nn.LogSoftmax(dim=1)
+                    )
 
 
 model.classifier = flower_classifier
 
 # Use GPU if it's available (I know it is, it is just to practice with general purpose code)
-device = torch.device("cuda" if torch.cuda.is_available()
-                      and args.gpu else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() and args.gpu else "cpu")
 # I stick to the Negative Logarithmic Likelihood Loss for discrete outputs
 criterion = nn.NLLLoss()
 # Adaptive momentum for the optimizer and quite low learning rate, at least to start off
@@ -143,6 +141,7 @@ steps = 0
 running_loss = 0
 
 for epoch in range(args.epochs):
+    
 
     for inputs, labels in trainloader:
         inputs, labels = inputs.to(device), labels.to(device)
@@ -173,7 +172,7 @@ for epoch in range(args.epochs):
                 top_p, top_class = ps.topk(1, dim=1)
                 equals = top_class == labels.view(*top_class.shape)
                 valid_accuracy += torch.mean(
-                    equals.type(torch.FloatTensor)).item()
+                equals.type(torch.FloatTensor)).item()
 
     print(f"Epoch {epoch+1}/{args.epochs}.. "
           f"Training loss: {running_loss/len(trainloader):.3f}.. "
@@ -189,7 +188,7 @@ model.class_to_idx = train_data.class_to_idx
 checkpoint = {'cnn': args.arch,
               'input_units': flower_classifier[0].in_features,
               'hidden_units': args.hidden_units,
-              'output_units': 102,
+              'output_units': len(train_data.class_to_idx),
               'dropout': 0.3,
               'state_dict': model.state_dict(),
               'optimizer_state': optimizer.state_dict(),
@@ -200,8 +199,7 @@ checkpoint = {'cnn': args.arch,
 parent_path = os.getcwd()+'/'
 if not os.path.exists(parent_path+args.checkpoint_save):
     os.makedirs(parent_path+args.checkpoint_save)
-new_path = os.path.join(
-    parent_path+args.checkpoint_save+'/flower_checkpoint.pth')
+new_path = os.path.join(parent_path+args.checkpoint_save+'/flower_checkpoint.pth')
 torch.save(checkpoint, new_path)
 
 print('Model training ended, checkpoint successfully saved')
